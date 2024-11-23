7fe19ef7d8958271a2007db6dbc7f3991193b5b3
Jseon-mi <pm3818@naver.com>
2024-11-23 16:37:05 +0900

    [server]세션 기능 추가

---
+++ SERVER/src/session_server.c
@@ -0,0 +1,65 @@
+#include "session.h"
+
+static Session *session_list = NULL;
+
+void generate_session_id(char *session_id){
+		const char *chars = "ABCDEFG...ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		for(int i = 0; i < MAX_LENGTH; i++){
+			session_id[i] = chars[rand() % strlen(chars)];
+		}
+		session_id[MAX_LENGTH] = '\0';
+}
+
+Session *create_session(const char *user_id){
+		Session *new_session = malloc(sizeof(Session));
+		if(new_session == NULL){
+			perror("malloc");
+			return NULL;
+		}
+
+		generate_session_id(new_session->session_id);
+		strncpy(new_session->user_id, user_id, MAX_LENGTH);
+		new_session->last_active = time(NULL);
+		new_session->next = session_list;
+		session_list = new_session;
+		return new_session;
+}
+
+Session *find_session(const char *session_id){
+	Session *cur = session_list;
+	while(cur !=  NULL){
+		if(strcmp(cur->session_id, session_id) == 0){	
+			return cur;
+		}
+		cur = cur->next;
+	}
+	return NULL;
+}
+
+void update_session(Session *session) {
+    session->last_active = time(NULL);
+}
+
+void remove_expired_sessions(){
+	Session *cur = session_list;
+	Session *prev = NULL;
+	time_t now = time(NULL);
+	
+	while(cur != NULL){
+		if(difftime(now, cur->last_active) > SESSION_TIME_OUT){
+				if(prev == NULL){
+					session_list = cur->next;
+				}
+				else{
+					prev->next = cur->next;
+				}
+				Session *temp = cur;
+				cur = cur->next;
+				free(temp);
+		}
+		else{
+			prev = cur;
+			cur = cur->next;
+		}
+	}
+}
