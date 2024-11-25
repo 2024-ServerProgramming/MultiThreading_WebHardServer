9845ac09a61271816b8b9dce736ea7019170dcd0
pchyuk <pch6590@gmail.com>
2024-11-25 15:58:16 +0900

    client.c 수정

--- client.c
+++ client.c
@@ -19,7 +19,7 @@ void errquit(char *msg) {
 }
 
 int main(int argc, char *argv[]) {
-    int sock;               // 서버와 연결된 소켓
+    int listen_sock;               // 서버와 연결된 소켓
     char command[MAXLINE];  // 사용자 명령어 저장
     char filename[MAXLINE], filepath[MAXLINE], dir[MAXLINE], buf[BUFSIZE];
     int success = 0;
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
     }
 
     // 서버에 연결
-    sock = tcp_connect(argv[1], atoi(argv[2]));
+    listen_sock = tcp_connect(argv[1], atoi(argv[2]));
 
     // 클라이언트 명령 루프
     while (1) {
@@ -39,7 +39,7 @@ int main(int argc, char *argv[]) {
         fgets(command, sizeof(command), stdin);
 
         // 서버에 명령어 전송
-        send(sock, command, strlen(command), 0);
+        send(listen_sock, command, strlen(command), 0);
 
         // 종료 명령 처리
         if (strncmp(command, "quit", 4) == 0) {
@@ -53,9 +53,9 @@ int main(int argc, char *argv[]) {
             fgets(dir, sizeof(dir), stdin);
             dir[strcspn(dir, "\n")] = 0;  // 개행 문자 제거
 
-            send(sock, dir, sizeof(dir), 0);  // 서버에 디렉토리 경로 전송
+            send(listen_sock, dir, sizeof(dir), 0);  // 서버에 디렉토리 경로 전송
 
-            recv(sock, &success, sizeof(success), 0);  // 결과 수신
+            recv(listen_sock, &success, sizeof(success), 0);  // 결과 수신
             if (success) printf("디렉토리 변경 성공\n");
             else printf("디렉토리 변경 실패\n");
         }
@@ -66,10 +66,10 @@ int main(int argc, char *argv[]) {
             fgets(filename, sizeof(filename), stdin);
             filename[strcspn(filename, "\n")] = 0;  // 개행 문자 제거
 
-            send(sock, filename, sizeof(filename), 0);  // 서버에 파일 이름 전송
+            send(listen_sock, filename, sizeof(filename), 0);  // 서버에 파일 이름 전송
 
             int file_exists;
-            recv(sock, &file_exists, sizeof(file_exists), 0);  // 파일 존재 여부 수신
+            recv(listen_sock, &file_exists, sizeof(file_exists), 0);  // 파일 존재 여부 수신
 
             if (!file_exists) {
                 printf("서버에서 파일을 찾을 수 없습니다.\n");
@@ -77,7 +77,7 @@ int main(int argc, char *argv[]) {
             }
 
             size_t file_size;
-            recv(sock, &file_size, sizeof(file_size), 0);  // 파일 크기 수신
+            recv(listen_sock, &file_size, sizeof(file_size), 0);  // 파일 크기 수신
 
             FILE *fp = fopen(filename, "wb");
             if (!fp) {
@@ -88,7 +88,7 @@ int main(int argc, char *argv[]) {
             printf("파일 다운로드 중...\n");
             size_t received = 0;
             while (received < file_size) {
-                int n = recv(sock, buf, BUFSIZE, 0);
+                int n = recv(listen_sock, buf, BUFSIZE, 0);
                 fwrite(buf, 1, n, fp);
                 received += n;
             }
@@ -106,30 +106,30 @@ int main(int argc, char *argv[]) {
             if (!fp) {
                 printf("파일을 찾을 수 없습니다: %s\n", filename);
                 int file_not_found = 1;
-                send(sock, &file_not_found, sizeof(file_not_found), 0);
+                send(listen_sock, &file_not_found, sizeof(file_not_found), 0);
                 continue;
             }
 
             int file_not_found = 0;
-            send(sock, &file_not_found, sizeof(file_not_found), 0);
-            send(sock, filename, sizeof(filename), 0);  // 서버에 파일 이름 전송
+            send(listen_sock, &file_not_found, sizeof(file_not_found), 0);
+            send(listen_sock, filename, sizeof(filename), 0);  // 서버에 파일 이름 전송
 
             fseek(fp, 0, SEEK_END);
             size_t file_size = ftell(fp);
             fseek(fp, 0, SEEK_SET);
 
-            send(sock, &file_size, sizeof(file_size), 0);  // 서버에 파일 크기 전송
+            send(listen_sock, &file_size, sizeof(file_size), 0);  // 서버에 파일 크기 전송
 
             printf("파일 업로드 중...\n");
             size_t sent = 0;
             while (sent < file_size) {
                 int n = fread(buf, 1, BUFSIZE, fp);
-                send(sock, buf, n, 0);
+                send(listen_sock, buf, n, 0);
                 sent += n;
             }
             fclose(fp);
 
-            recv(sock, &success, sizeof(success), 0);  // 업로드 결과 수신
+            recv(listen_sock, &success, sizeof(success), 0);  // 업로드 결과 수신
             if (success) printf("파일 업로드 성공: %s\n", filename);
             else printf("파일 업로드 실패\n");
         }
@@ -138,7 +138,7 @@ int main(int argc, char *argv[]) {
         else if (strncmp(command, "tree", 4) == 0) {
             printf("서버 파일 구조:\n");
             while (1) {
-                int n = recv(sock, buf, BUFSIZE, 0);
+                int n = recv(listen_sock, buf, BUFSIZE, 0);
                 if (n <= 0) break;
                 buf[n] = '\0';
                 printf("%s", buf);
@@ -155,16 +155,16 @@ int main(int argc, char *argv[]) {
             fgets(filename, sizeof(filename), stdin);
             filename[strcspn(filename, "\n")] = 0;  // 개행 문자 제거
 
-            send(sock, filepath, sizeof(filepath), 0);  // 서버에 경로 전송
-            send(sock, filename, sizeof(filename), 0);  // 서버에 파일 이름 전송
+            send(listen_sock, filepath, sizeof(filepath), 0);  // 서버에 경로 전송
+            send(listen_sock, filename, sizeof(filename), 0);  // 서버에 파일 이름 전송
 
-            recv(sock, &success, sizeof(success), 0);  // 삭제 결과 수신
+            recv(listen_sock, &success, sizeof(success), 0);  // 삭제 결과 수신
             if (success) printf("파일 삭제 성공: %s/%s\n", filepath, filename);
             else printf("파일 삭제 실패\n");
         }
     }
 
-    close(sock);
+    close(listen_sock);
     return 0;
 }
 
