204533b7cc03342d8634f316f241b24982a144bf
Wonsick Choi <king258436@naver.com>
2024-12-01 12:27:15 +0900

    서버 켰을떄 로그, 클라이언트 연결, 해제에 번호 달고 로그찍게함.(연결됐는지 확인 잘 안돼서) ./bin/server.exe 8080 서버 실행완료 클라이언트 연결요청 기다리는중 .. 클라이언트 0 연결 완료 Client 0 disconnected 클라이언트 1 연결 완료 Client 1 disconnected

--- SERVER/include/server.h
+++ SERVER/include/server.h
@@ -1,10 +1,10 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include "session.h"
 #include "server_config.h"
-#include <pthread.h>
+#include "session.h"
 #include <arpa/inet.h>
+#include <pthread.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #define MAX 10
@@ -18,10 +18,15 @@ typedef struct {
 
 /* 클라이언트 세션 구조체 */
 typedef struct {
-    int cli_data;       // 클라이언트 소켓
-    int is_login;       // 로그인 여부
-    Session *session;   // 세션 포인터 (로그인된 사용자의 세션 참조)
+    int cli_data;     // 클라이언트 소켓
+    int is_login;     // 로그인 여부
+    Session *session; // 세션 포인터 (로그인된 사용자의 세션 참조)
 } CliSession;
+/* 메인에서 쓰래드 호출할때 보낼 구조체 */
+typedef struct {
+    int sock;     // 소켓 이름
+    int user_num; // 유저 번호
+} ThreadArgs;
 
 /* 파일 offset 구조체 */
 typedef struct offset_info {
@@ -41,4 +46,4 @@ void sign_up(CliSession *session);
 void *client_handle(CliSession *cliS);
 void *client_thread(void *arg);
 
-#endif 
+#endif
--- SERVER/src/server_main.c
+++ SERVER/src/server_main.c
@@ -1,15 +1,15 @@
 #include "server.h"
 
-int tcp_listen(int host, int port, int backlog){
+int tcp_listen(int host, int port, int backlog) {
     int sd = socket(AF_INET, SOCK_STREAM, 0);
 
-     /* 포트 중복 사용 해결 */
+    /* 포트 중복 사용 해결 */
     int optvalue = 1;
     setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optvalue, sizeof(optvalue));
 
-    if(sd == -1){
-            perror("socket");
-            exit(1);
+    if (sd == -1) {
+        perror("socket");
+        exit(1);
     }
     struct sockaddr_in sin;
 
@@ -18,12 +18,12 @@ int tcp_listen(int host, int port, int backlog){
     sin.sin_addr.s_addr = htonl(host);
     sin.sin_port = htons(port);
 
-    if(bind(sd, (struct sockaddr *)&sin, sizeof(sin))){
-            perror("Bind");
-            exit(1);
+    if (bind(sd, (struct sockaddr *)&sin, sizeof(sin))) {
+        perror("Bind");
+        exit(1);
     }
 
-    if(listen(sd, backlog) < 0){
+    if (listen(sd, backlog) < 0) {
         perror("listen failed...");
         exit(1);
     }
@@ -31,63 +31,60 @@ int tcp_listen(int host, int port, int backlog){
     return sd;
 }
 
-void start_index(int cli_sock){
+void start_index(ThreadArgs arg) {
     char buf[BUFSIZE];
     int rsize;
 
     CliSession cliS;
     memset(&cliS, 0, sizeof(CliSession));
-    cliS.cli_data = cli_sock;
+    cliS.cli_data = arg.sock;
     cliS.is_login = 0;
 
-    while((rsize = recv(cli_sock, buf, sizeof(buf), 0)) > 0){
+    while ((rsize = recv(arg.sock, buf, sizeof(buf), 0)) > 0) {
         buf[rsize] = '\0';
 
-        if(strcmp(buf, "SignUp") == 0){
+        if (strcmp(buf, "SignUp") == 0) {
             sign_up(&cliS);
-        }
-        else if(strcmp(buf, "SignIn") == 0){
+        } else if (strcmp(buf, "SignIn") == 0) {
             strcpy(buf, "SignInOK");
             send(cliS.cli_data, buf, strlen(buf), 0);
 
-            rsize = recv(cli_sock, buf, sizeof(buf) - 1, 0);
-            if(rsize <= 0){
+            rsize = recv(arg.sock, buf, sizeof(buf) - 1, 0);
+            if (rsize <= 0) {
                 perror("ID and PW failed");
                 break;
             }
             buf[rsize] = '\0';
 
             sign_in(&cliS, buf);
-            if(cliS.is_login){
+            if (cliS.is_login) {
                 client_handle(&cliS);
                 break;
             }
-        }
-        else{
+        } else {
             char temp_buf[BUFSIZE];
             snprintf(temp_buf, sizeof(temp_buf), "Unknown command: %s", buf);
             strncpy(buf, temp_buf, sizeof(buf) - 1);
             send(cliS.cli_data, buf, strlen(buf), 0);
-            memset(buf, 0, sizeof(buf)); 
+            memset(buf, 0, sizeof(buf));
         }
     }
 
-    if(rsize == 0){
-        printf("Client disconnected\n");
-    } 
-    else if(rsize == -1){
+    if (rsize == 0) {
+        printf("Client %d disconnected\n", arg.user_num);
+    } else if (rsize == -1) {
         perror("recv");
         exit(1);
     }
     close(cliS.cli_data);
 }
 
-int main(int argc, char* argv[]){
-    if(argc!=2){
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         printf("%s <port> 로 입력해주세요.\n", argv[0]);
         exit(1);
     }
-    int port= atoi(argv[1]);
+    int port = atoi(argv[1]);
     struct sockaddr_in cli;
     int listen_sock;
     socklen_t cli_len = sizeof(cli);
@@ -97,36 +94,44 @@ int main(int argc, char* argv[]){
 
     srand(time(NULL)); // 접속 시간을 위한 난수 생성
 
-    if(pthread_mutex_init(&m_lock, NULL) != 0){ 
+    if (pthread_mutex_init(&m_lock, NULL) != 0) {
         perror("Mutex Init Failure");
         return 1;
     }
-    
-    while(1){
+    int user = 0;
+    printf("서버 실행완료 클라이언트 연결요청 기다리는중 ..\n");
+    while (1) { // 유저마다 소켓 만들어줌
         int *cli_sock = malloc(sizeof(int));
         *cli_sock = accept(listen_sock, (struct sockaddr *)&cli, &cli_len);
-        if(*cli_sock < 0){
-            perror("Accept");
+
+        if (*cli_sock < 0) {
+            perror("소켓 연결 에러");
             free(cli_sock);
             continue;
+        } else { // 연결 성공했을때 서버에 로그 띄워줌
+            printf("클라이언트 %d 연결 완료\n", user);
         }
+        ThreadArgs *arg = malloc(sizeof(ThreadArgs));
+        arg->sock = *cli_sock;
+        arg->user_num = user;
 
         pthread_t tid;
-        if(pthread_create(&tid, NULL, client_thread, (void *)cli_sock) != 0){
+        if (pthread_create(&tid, NULL, client_thread, (void *)arg) != 0) {
             perror("pthread_create");
             free(cli_sock);
         }
         pthread_detach(tid);
+        user++;
     }
 
     close(listen_sock);
     return 0;
 }
 
-void *client_thread(void *arg){
-    int cli_sock = *((int *)arg);
+void *client_thread(void *arg) {
+    ThreadArgs client_arg = *((ThreadArgs *)arg);
     free(arg);
 
-    start_index(cli_sock);
+    start_index(client_arg);
     pthread_exit(NULL);
 }
